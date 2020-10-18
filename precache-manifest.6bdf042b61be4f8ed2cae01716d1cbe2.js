b61b9e885e7f70c9678567c548c23d426b4f0634
water <0529bill@gmail.com>
2020-10-18 17:12:51 +0800

    Updates

---
+++ precache-manifest.6bdf042b61be4f8ed2cae01716d1cbe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61f5a10fb117dc9771d9ec6ac4902ccc",
+    "url": "/youtube_practice/index.html"
+  },
+  {
+    "revision": "2479a56ef9cd35b7a934",
+    "url": "/youtube_practice/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "2479a56ef9cd35b7a934",
+    "url": "/youtube_practice/static/js/2.c1e89c51.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/youtube_practice/static/js/2.c1e89c51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfde2965c0f0f3031f34",
+    "url": "/youtube_practice/static/js/main.b1c8cb52.chunk.js"
+  },
+  {
+    "revision": "8f8a2768e9501f9de076",
+    "url": "/youtube_practice/static/js/runtime-main.8811fd57.js"
+  }
+]);
